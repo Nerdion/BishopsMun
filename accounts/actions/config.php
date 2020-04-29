87cc618c333d3204ff85d6e5425949906f70d3dd
Neel Khalade <neel99khalade@gmail.com>
2020-04-29 19:02:35 +0530

    Update config.php

--- accounts/actions/config.php
+++ accounts/actions/config.php
@@ -10,8 +10,8 @@ class Database {
     function getConnection() {
         
         $this->servername = "localhost";
-        $this->username = "u752327893_maindb";
-        $this->password = "Chaicreamroll24@";
+        $this->username = "root";
+        $this->password = "";
         $this->dbname = "u752327893_mun";
         // Create connection
         $con = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
